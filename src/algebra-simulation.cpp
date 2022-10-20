1a73f45901b9bb81d128dbc6743d418d2ba0455b
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-10-20 23:42:51 +0200

    implement MPI communication skeleton

--- src/algebra-simulation.cpp
+++ src/algebra-simulation.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <memory>
 #include <mpi.h>
+#include <regex>
 #include <string>
+
+const std::regex extract_rows = std::regex("sum \\[(\\d+),(\\d+)\\]");
+constexpr int GRACEFUL_STOP = -1;
+constexpr int ERROR_STOP = -2;
+
+std::unique_ptr<int[]> stop_array(int r, bool graceful);
 
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
@@ -8,12 +16,60 @@ int main(int argc, char **argv) {
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  std::string user_input;
+  int r;
+  MPI_Comm_size(MPI_COMM_WORLD, &r);
 
-  std::cin >> user_input;
-  while (rank != "quit") {
-    // TODO
+  int status = 0;
+
+  while (true) {
+    std::unique_ptr<int[]> send_buffer = nullptr;
+    std::unique_ptr<int[]> recv_buffer = std::unique_ptr<int[]>(new int[2]);
+
+    if (rank == 0) {
+      std::smatch match;
+      std::string user_input;
+      std::getline(std::cin, user_input);
+      if (regex_search(user_input, match, extract_rows)) {
+        std::cout << match.str(1) << ", " << match.str(2) << std::endl;
+
+        int *send_buffer_data = new int[2 * r];
+        for (int i = 0; i < 2 * r; ++i) {
+          send_buffer_data[i] = 0;
+        }
+        send_buffer = std::unique_ptr<int[]>(send_buffer_data);
+      } else {
+        bool error = user_input != "quit";
+        if (error) {
+          std::cerr << "Invalid input" << std::endl;
+        }
+        send_buffer = stop_array(r, !error);
+      }
+    }
+
+    MPI_Scatter(send_buffer.get(), 2, MPI_INT, recv_buffer.get(), 2, MPI_INT, 0,
+                MPI_COMM_WORLD);
+    status = recv_buffer[0];
+    if (status < 0) {
+      break;
+    }
+
+    std::cout << recv_buffer[0] << ", " << recv_buffer[1] << " (" << rank << ")"
+              << std::endl;
   }
 
   MPI_Finalize();
+
+  if (status == ERROR_STOP && rank == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+std::unique_ptr<int[]> stop_array(int r, bool graceful) {
+  int *array = new int[2 * r];
+  for (int i = 0; i < 2 * r; ++i) {
+    array[i] = graceful ? GRACEFUL_STOP : ERROR_STOP;
+  }
+  return std::unique_ptr<int[]>(array);
 }
